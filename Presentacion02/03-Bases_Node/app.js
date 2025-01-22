66f27af86e70f9976637b03719ea55a14bd478c9
Ka1laK <rissel1771@gmail.com>
2025-01-22 00:13:38 -0500

    organizacion_archivos

---
+++ Presentacion02/03-Bases_Node/app.js
@@ -0,0 +1,8 @@
+console.clear()
+console.log("===============")
+console.log(" Tabla de 5")
+console.log("===============")
+const j = 5
+for (let i = 1; i <= 10; i++) {
+    console.log(`${j} x ${i} = ${j * i}`)
+}
