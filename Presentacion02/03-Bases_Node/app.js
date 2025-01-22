41a0026aac03a6ca16d352fbe54ccf68e257e3fe
Ka1laK <rissel1771@gmail.com>
2025-01-22 13:47:29 -0500

    laboratorio_2-Bases_Node

--- Presentacion02/03-Bases_Node/app.js
+++ Presentacion02/03-Bases_Node/app.js
@@ -1,8 +1,29 @@
-console.clear()
-console.log("===============")
-console.log(" Tabla de 5")
-console.log("===============")
-const j = 5
-for (let i = 1; i <= 10; i++) {
-    console.log(`${j} x ${i} = ${j * i}`)
-}
+// Hacer la tabla del X en JavaScript
+const path = require('path');
+const { crearArchivo } = require(path.join(__dirname, 'helper', 'multiplicar.js'));
+const argv = require(path.join(__dirname, 'config', 'yargs.js'));
+const colors = require('colors');
+
+console.clear();
+
+crearArchivo(argv.b, argv.l, argv.h)
+    .then( nombreArchivo => console.log(nombreArchivo.rainbow, 'Creado'))
+    .catch( err => console.log(err));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
